f33a6438f00cb84b4678ff50849e5c791225187d
heoyoon <parechae@gmail.com>
2023-09-05 15:36:38 +0900

    인풋액션 추가

---
+++ TestingZone/Assets/HeoyoonScript/RoundSlider.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoundSlider : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+public class SelectUI
+{
+    public UnityEngine.UI.Image[] images = new UnityEngine.UI.Image[8];
+    public UnityEngine.UI.Image pickedImages;
+    public byte pickedUI;
+}
